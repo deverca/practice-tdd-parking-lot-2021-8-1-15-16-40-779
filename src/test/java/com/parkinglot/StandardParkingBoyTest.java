b25f5bf04809b808cd8b357ef188c3c70bab3b75
deverca <deverca@oocl.com>
2021-08-07 13:34:19 +0800

    [STORY 3][CARMELA]refactor: renamed test case to should_return_ticket_when_park_given_a_parking_lot_parking_boy_and_a_car

--- src/test/java/com/parkinglot/StandardParkingBoyTest.java
+++ src/test/java/com/parkinglot/StandardParkingBoyTest.java
@@ -9,7 +9,7 @@ public class StandardParkingBoyTest {
 
 
     @Test
-    public void should_return_ticket_when_park_given_a_parking_lot_and_a_car() {
+    public void should_return_ticket_when_park_given_a_parking_lot_parking_boy_and_a_car() {
         //given
         StandardParkingBoy standardParkingBoy = new StandardParkingBoy(new ParkingLot());
         Car car = new Car();
